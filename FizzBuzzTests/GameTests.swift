0cd7e3db8d409af17e15c0761544db1af35b0c1f
Oliver Cripps <ollycripps@gmail.com>
2019-06-10 23:21:00 +0100

    Implement check move against Brain function

--- FizzBuzzTests/GameTests.swift
+++ FizzBuzzTests/GameTests.swift
@@ -18,14 +18,62 @@ class GameTests: XCTestCase {
     }
     
     func testOnPlayScoreIncremented() {
-        game.play()
+        _ = game.play(move: "1")
         XCTAssertTrue(game.score == 1)
     }
     
     func testOnPlayTwiceScoreIncremented() {
         game.score = 1
-        game.play()
+        _ = game.play(move: "2")
         XCTAssertTrue(game.score == 2)
+    }
+    
+    func testIfFizzMoveIsRight() {
+        game.score = 2
+        let result = game.play(move: "Fizz")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIfFizzMoveIsWrong() {
+        game.score = 1
+        let result = game.play(move: "Fizz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIfBuzzMoveIsRight() {
+        game.score = 4
+        let result = game.play(move: "Buzz")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsBuzzMoveisWrong() {
+        game.score = 7
+        let result = game.play(move: "Buzz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsFizzBuzzMoveRight() {
+        game.score = 14
+        let result = game.play(move: "FizzBuzz")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsFizzBuzzMoveWrong() {
+        game.score = 12
+        let result = game.play(move: "FizzBuzz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testIsNumberMoveRight() {
+        game.score = 6
+        let result = game.play(move: "7")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsNumberMoveWrong() {
+        game.score = 4
+        let result = game.play(move: "5")
+        XCTAssertEqual(result, false)
     }
 
 }
