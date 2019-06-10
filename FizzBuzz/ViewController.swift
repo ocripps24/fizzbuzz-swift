d4af35af9f99395e6634827f18fe32bbb20a88f9
Oliver Cripps <ollycripps@gmail.com>
2019-06-10 21:22:56 +0100

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Oliver Cripps on 10/06/2019.
+//  Copyright © 2019 Oliver Cripps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
